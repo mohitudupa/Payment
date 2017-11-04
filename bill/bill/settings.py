228649802e1bb86de7db0fdd505c59ad78a1bbea
mohitudupa <33372461+mohitudupa@users.noreply.github.com>
2017-11-04 20:18:22 +0530

    Update settings.py

--- bill/bill/settings.py
+++ bill/bill/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = ['192.168.0.100', '127.0.0.1', '192.168.43.186', '192.168.0.105'
 # Application definition
 
 INSTALLED_APPS = [
-    'easy_pdf',
     'payment.apps.PaymentConfig',
     'django.contrib.admin',
     'django.contrib.auth',
