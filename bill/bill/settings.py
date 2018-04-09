b05080adc0a938f65737de54a2e89c5fb851e600
mohitudupa <33372461+mohitudupa@users.noreply.github.com>
2018-04-10 01:49:14 +0530

    2.0

--- bill/bill/settings.py
+++ bill/bill/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5qe^syp+2@w*wu1qn5y@oycgrqi^1-zcn9yl9&wi@!ncg7!ukj'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
